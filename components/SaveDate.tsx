e5ec85648e7e1e42f591c65b332525ea593b1585
DL Salo <salo@objectsonore.com>
2024-10-09 10:27:42 -0700

    reroute setup

---
+++ components/SaveDate.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image"
+import std from "@/images/save-the-date.png"
+import Flip from "@/components/Flip"
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+const SaveDate = () => {
+  const targetDate = dayjs.tz("2024-11-02 10:00", "America/Los_Angeles")
+  const timestamp = targetDate.valueOf()
+  return (
+    <div className="w-full">
+      <div className="bg-[url('../images/background-orange.png')] bg-cover text-[#931136] bg-left  border-[#931136] md:border-[10px] md:rounded-[35px]  p-8 m-2 border-4 rounded-md">
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 items-center w-full">
+          <div className="flex flex-col gap-4">
+            <Image
+              src={std}
+              alt="Save the Date"
+              width={300}
+              height={100}
+              sizes="100vw"
+              className="hidden md:block"
+            />
+            <Image
+              src={std}
+              alt="Save the Date"
+              width={200}
+              height={75}
+              sizes="100vw"
+              className="md:hidden"
+            />
+            <div className="font-extrabold md:text-lg leading-6">
+              The festivities of the CCBP shindig kick off bright and early on Nov 2, 2024 at 10am.
+              Boy, oh boy, we&apos;re just bursting with excitement to have you there!
+            </div>
+          </div>
+
+          <div className="lg:hidden block items-center justify-self-center bg-white p-2 rounded-sm bg-opacity-75">
+            <Flip
+              labelStyle={{
+                fontSize: 10,
+                fontWeight: 500,
+                textTransform: "uppercase",
+                color: "black",
+              }}
+              digitBlockStyle={{
+                width: 20,
+                height: 30,
+                fontSize: 22,
+                borderRadius: 3,
+              }}
+              to={timestamp}
+            />
+          </div>
+          <div className="lg:block hidden items-center justify-self-center bg-white p-2 rounded-sm bg-opacity-75">
+            <Flip
+              labelStyle={{
+                fontSize: 10,
+                fontWeight: 500,
+                textTransform: "uppercase",
+                color: "black",
+              }}
+              digitBlockStyle={{
+                width: 30,
+                height: 45,
+                fontSize: 22,
+                borderRadius: 3,
+              }}
+              to={timestamp}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SaveDate
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ['www.revengeof.com'],
   },
+  async redirects() {
+    return [
+      // Redirect to current CCBP year
+      {
+        source: '/',
+        destination: '/ccbp-2024',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
