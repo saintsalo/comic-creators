153205358a1353aaba3b8c7dfb52b4600ab0c1de
DL Salo <salo@objectsonore.com>
2024-10-08 20:37:18 -0700

    css fixes

--- app/page.tsx
+++ app/page.tsx
@@ -26,64 +26,66 @@ export default function Home() {
           priority
         />
       </div>
-      <div className="w-full bg-[url('../images/background-orange.png')] bg-cover md:px-12 md:py-4 text-[#A20035] bg-left  border-[#A20035] md:border-[10px] md:rounded-[35px] rounded-md p-4">
-        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 items-center">
-          <div className="flex flex-col gap-4">
-            <Image
-              src={std}
-              alt="Save the Date"
-              width={300}
-              height={100}
-              sizes="100vw"
-              className="hidden md:block"
-            />
-            <Image
-              src={std}
-              alt="Save the Date"
-              width={200}
-              height={75}
-              sizes="100vw"
-              className="md:hidden"
-            />
-            <div className="font-extrabold md:text-lg leading-6">
-              The festivities of the CCBP shindig kick off bright and early on Nov 2, 2024 at 10am.
-              Boy, oh boy, we&apos;re just bursting with excitement to have you there!
+      <div className="w-full">
+        <div className="bg-[url('../images/background-orange.png')] bg-cover text-[#A20035] bg-left  border-[#A20035] md:border-[10px] md:rounded-[35px] rounded-md p-8 m-2">
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 items-center w-full">
+            <div className="flex flex-col gap-4">
+              <Image
+                src={std}
+                alt="Save the Date"
+                width={300}
+                height={100}
+                sizes="100vw"
+                className="hidden md:block"
+              />
+              <Image
+                src={std}
+                alt="Save the Date"
+                width={200}
+                height={75}
+                sizes="100vw"
+                className="md:hidden"
+              />
+              <div className="font-extrabold md:text-lg leading-6">
+                The festivities of the CCBP shindig kick off bright and early on Nov 2, 2024 at
+                10am. Boy, oh boy, we&apos;re just bursting with excitement to have you there!
+              </div>
             </div>
-          </div>
 
-          <div className="lg:hidden block items-center justify-self-center bg-white p-2 rounded-sm bg-opacity-75">
-            <Flip
-              labelStyle={{
-                fontSize: 10,
-                fontWeight: 500,
-                textTransform: "uppercase",
-                color: "black",
-              }}
-              digitBlockStyle={{
-                width: 20,
-                height: 30,
-                fontSize: 22,
-                borderRadius: 3,
-              }}
-              to={timestamp}
-            />
-          </div>
-          <div className="lg:block hidden items-center justify-self-center bg-white p-2 rounded-sm bg-opacity-75">
-            <Flip
-              labelStyle={{
-                fontSize: 10,
-                fontWeight: 500,
-                textTransform: "uppercase",
-                color: "black",
-              }}
-              digitBlockStyle={{
-                width: 30,
-                height: 45,
-                fontSize: 22,
-                borderRadius: 3,
-              }}
-              to={timestamp}
-            />
+            <div className="lg:hidden block items-center justify-self-center bg-white p-2 rounded-sm bg-opacity-75">
+              <Flip
+                labelStyle={{
+                  fontSize: 10,
+                  fontWeight: 500,
+                  textTransform: "uppercase",
+                  color: "black",
+                }}
+                digitBlockStyle={{
+                  width: 20,
+                  height: 30,
+                  fontSize: 22,
+                  borderRadius: 3,
+                }}
+                to={timestamp}
+              />
+            </div>
+            <div className="lg:block hidden items-center justify-self-center bg-white p-2 rounded-sm bg-opacity-75">
+              <Flip
+                labelStyle={{
+                  fontSize: 10,
+                  fontWeight: 500,
+                  textTransform: "uppercase",
+                  color: "black",
+                }}
+                digitBlockStyle={{
+                  width: 30,
+                  height: 45,
+                  fontSize: 22,
+                  borderRadius: 3,
+                }}
+                to={timestamp}
+              />
+            </div>
           </div>
         </div>
       </div>
