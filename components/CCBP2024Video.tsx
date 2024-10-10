577b92c89679a85e145b689a15b09d4bf1689630
DL Salo <salo@objectsonore.com>
2024-10-09 17:38:20 -0700

    video update link

--- components/CCBP2024Video.tsx
+++ components/CCBP2024Video.tsx
@@ -5,7 +5,7 @@ export default function CCBP2024Video() {
       <div className="w-full">
         <iframe
           className="lg:border-8 border-2 border-[#931136] lg:rounded-[35px] rounded-md aspect-video w-[100%]"
-          src="https://www.youtube.com/embed/GyBmrNnNVWU?si=zhywubKCiJJhBd90"
+          src="https://www.youtube.com/embed/zt3j4q6rUU4?si=7ImDXQFsSb_MTLRz"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
