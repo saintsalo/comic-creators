20750ff924ca6da77be0de37767b9449ee960ade
DL Salo <salo@objectsonore.com>
2024-10-10 12:31:49 -0700

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { anton_init, roboto_init } from "@/fonts"
 import "./globals.css"
 import Script from "next/script"
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 export const metadata: Metadata = {
   title: "Comic Creators Block Party",
@@ -20,6 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${roboto_init.className} ${anton_init.variable} antialiased`}>
+        {process.env.NODE_ENV === "production" ? <GoogleAnalytics gaId="G-3PJ2SP5PVW" /> : null}
+        <GoogleAnalytics gaId="G-3PJ2SP5PVW" />
         <Script
           id="klaviyo"
           type="text/javascript"
